a5b27b8d368c34e5ef539c43c5dd5c350170eaf9
eglezg <ernestoglezg9182@hotmail.com>
2024-07-03 23:26:17 -0600

    Base de datos dislexiakids_db

---
+++ db/dislexiakids_db.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-07-2024 a las 06:01:45
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dislexiakids_db`
+--
+CREATE DATABASE IF NOT EXISTS `dislexiakids_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `dislexiakids_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evaluación`
+--
+
+DROP TABLE IF EXISTS `evaluación`;
+CREATE TABLE IF NOT EXISTS `evaluación` (
+  `idEvaluacion` int(11) NOT NULL,
+  `Nombre` varchar(30) DEFAULT NULL,
+  `Descripción` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idEvaluacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `institución`
+--
+
+DROP TABLE IF EXISTS `institución`;
+CREATE TABLE IF NOT EXISTS `institución` (
+  `idInstitucion` int(11) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `numero` int(12) DEFAULT NULL,
+  `correo` varchar(30) DEFAULT NULL,
+  `ubicacion` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idInstitucion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pregunta`
+--
+
+DROP TABLE IF EXISTS `pregunta`;
+CREATE TABLE IF NOT EXISTS `pregunta` (
+  `idPregunta` int(11) NOT NULL,
+  `idEvaluacion` int(11) DEFAULT NULL,
+  `pregunta` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`idPregunta`),
+  KEY `idEvaluacion` (`idEvaluacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reporte`
+--
+
+DROP TABLE IF EXISTS `reporte`;
+CREATE TABLE IF NOT EXISTS `reporte` (
+  `idReporte` int(11) NOT NULL,
+  `idEvaluacion` int(11) DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `idInstitucion` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `resultado` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idReporte`),
+  KEY `idEvaluacion` (`idEvaluacion`),
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idInstitucion` (`idInstitucion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuesta`
+--
+
+DROP TABLE IF EXISTS `respuesta`;
+CREATE TABLE IF NOT EXISTS `respuesta` (
+  `idRespuesta` int(11) NOT NULL,
+  `idPregunta` int(11) DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `respuesta` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`idRespuesta`),
+  KEY `idPregunta` (`idPregunta`),
+  KEY `idUsuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `nombre` varchar(40) DEFAULT NULL,
+  `correo` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  ADD CONSTRAINT `pregunta_ibfk_1` FOREIGN KEY (`idEvaluacion`) REFERENCES `evaluación` (`idEvaluacion`);
+
+--
+-- Filtros para la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  ADD CONSTRAINT `reporte_ibfk_1` FOREIGN KEY (`idEvaluacion`) REFERENCES `evaluación` (`idEvaluacion`),
+  ADD CONSTRAINT `reporte_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `reporte_ibfk_3` FOREIGN KEY (`idInstitucion`) REFERENCES `institución` (`idInstitucion`);
+
+--
+-- Filtros para la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`idPregunta`) REFERENCES `pregunta` (`idPregunta`),
+  ADD CONSTRAINT `respuesta_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
