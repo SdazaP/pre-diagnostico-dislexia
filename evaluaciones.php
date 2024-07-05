018dc12f97bb4198904e38bef6b0be7e60d4c2f6
eglezg <ernestoglezg9182@hotmail.com>
2024-07-05 12:29:27 -0600

    Interfaz Evaluaciones con tarjetas

--- evaluaciones.php
+++ evaluaciones.php
@@ -8,9 +8,63 @@
                 repellendus laudantium voluptatum. Blanditiis?</p>
         </div>
     </section>
+
+    <section>
+        <div class="eval-container">
+            <div class="row">
+                <div class="col-md-6 mb-4">
+                    <div class="eval-card">
+                        <img src="images/causas.jpeg" class="eval-card-img-top">
+                        <div class="eval-card-body">
+                            <h3 class="eval-card-title">Título de la Tarjeta</h3>
+                            <p class="eval-card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus molestiae dolorem eius velit
+                tempore optio quis fuga necessitatibus quae autem. Consectetur dicta ducimus ipsa laborum! Doloribus
+                repellendus laudantium voluptatum. Blanditiis?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <div class="eval-card">
+                        <img src="images/consecuencias.jpeg" class="eval-card-img-top">
+                        <div class="eval-card-body">
+                            <h3 class="eval-card-title">Título de la Tarjeta</h3>
+                            <p class="eval-card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus molestiae dolorem eius velit
+                tempore optio quis fuga necessitatibus quae autem. Consectetur dicta ducimus ipsa laborum! Doloribus
+                repellendus laudantium voluptatum. Blanditiis?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <div class="eval-card">
+                        <img src="images/mitos.jpeg" class="eval-card-img-top">
+                        <div class="eval-card-body">
+                            <h3 class="eval-card-title">Título de la Tarjeta</h3>
+                            <p class="eval-card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus molestiae dolorem eius velit
+                tempore optio quis fuga necessitatibus quae autem. Consectetur dicta ducimus ipsa laborum! Doloribus
+                repellendus laudantium voluptatum. Blanditiis?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <div class="eval-card">
+                        <img src="images/Logo.png" class="eval-card-img-top">
+                        <div class="eval-card-body">
+                            <h3 class="eval-card-title">Título de la Tarjeta</h3>
+                            <p class="eval-card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus molestiae dolorem eius velit
+                tempore optio quis fuga necessitatibus quae autem. Consectetur dicta ducimus ipsa laborum! Doloribus
+                repellendus laudantium voluptatum. Blanditiis?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="eval-btn">
+                <a class="btn btn-primary ini-btn" href="#" role="button">Iniciar Evaluación</a>
+            </div>
+        </div>
+    </section>
     
     <!-- Carrusel de Evaluaciones  -->
-    <section class="container mt-4 carrusel">
+    <!--<section class="container mt-4 carrusel">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -46,6 +100,6 @@
                 <span class="sr-only">Siguiente</span>
             </a>
         </div>
-    </section>
+    </section> -->
 
 <?php include("template/footer.php")?>
