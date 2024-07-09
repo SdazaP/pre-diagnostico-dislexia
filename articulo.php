d99848e8884c4275a5a53d01fda5d5cebfde3ac7
eglezg <ernestoglezg9182@hotmail.com>
2024-07-09 13:15:40 -0600

    Información artículo

--- articulo.php
+++ articulo.php
@@ -1,30 +1,188 @@
-<?php include("template/header.php")?>
+<?php include("template/header.php") ?>
 
-    <!-- Sección: ¿Qué es la dislexia? -->
-    <section class="container mt-4 art">
-        <div>
-            <h1 class="titulo">¿Qué es la dislexia?</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus molestiae dolorem eius velit
-                tempore optio quis fuga necessitatibus quae autem. Consectetur dicta ducimus ipsa laborum! Doloribus
-                repellendus laudantium voluptatum. Blanditiis?</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus molestiae dolorem eius velit
-                tempore optio quis fuga necessitatibus quae autem. Consectetur dicta ducimus ipsa laborum! Doloribus
-                repellendus laudantium voluptatum. Blanditiis?</p>
-        </div>
-    </section>
-    <!-- Sección: Recomendaciones  -->
-    <section class="container mt-4 art">
-        <div>
-            <h1 class="titulo">Recomendaciones</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus molestiae dolorem eius velit
-                tempore optio quis fuga necessitatibus quae autem. Consectetur dicta ducimus ipsa laborum! Doloribus
-                repellendus laudantium voluptatum. Blanditiis?</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus molestiae dolorem eius velit
-                tempore optio quis fuga necessitatibus quae autem. Consectetur dicta ducimus ipsa laborum! Doloribus
-                repellendus laudantium voluptatum. Blanditiis?</p>
-        </div>
+<!-- Sección: ¿Qué es la dislexia? -->
+<section class="container mt-4 art">
+    <div>
+        <h1 class="titulo">¿Qué es la dislexia?</h1>
+        <p>La dislexia es una dificultad específica de aprendizaje que forma parte de las dificultades específicas de aprendizaje (DEA), caracterizadas por problemas en procesos básicos como la comprensión oral y escrita del lenguaje. Afecta áreas como pensamiento, habla, lectura, escritura, deletreo y el manejo de signos matemáticos. Aunque no está relacionada con un déficit cognitivo ni sensorial evidente, puede ser causa de bajo rendimiento escolar y dificultades sociales.</p>
+        <br>
+        <p>Es el trastorno más común en la lectura y aprendizaje, afectando aproximadamente al 2-8% de los niños escolarizados, siendo más prevalente en niños que en niñas y con antecedentes familiares frecuentes. Suele manifestarse junto con trastornos como la discalculia y la disgrafía. Los niños con dislexia pueden también presentar problemas de atención e impulsividad.</p>
+        <br>
+        <p>El diagnóstico de la dislexia se basa en la observación de un retraso notable en la adquisición de habilidades de lectura en comparación con sus compañeros. Se relaciona con dificultades en el procesamiento fonológico, auditivo y visual, así como con déficits en la memoria de trabajo y en el conocimiento sintáctico.</p>
+        <br>
+        <p>Además, existen variantes como la disgrafía, que afecta la habilidad para escribir correctamente, y la discalculia, que se refiere a dificultades en el cálculo y el manejo de números. Estos trastornos pueden tener un impacto significativo en el rendimiento académico y emocional de los niños afectados, afectando no solo su desempeño escolar sino también su adaptación social y emocional.</p>
+    </div>
+</section>
+
+<!-- Sección: Recomendaciones  -->
+<section class="container mt-4 art">
+    <div>
+        <h1 class="titulo">Recomendaciones</h1>
+        <h3 class="subtitulo">Papel de la familia</h3>
+        <br>
+        <p>En nuestro sistema educativo se da por supuesto que la responsabilidad de la
+            enseñanza recae sobre el profesor más que sobre los padres; y en el caso de los
+            niños disléxicos, se suele derivar hacia el especialista (psicólogo, pedagogo,
+            logopeda o profesor especializado). Esto no es así, puesto que en todos los casos,
+            y mucho más en el de los niños disléxicos, sin el apoyo directo de la familia,
+            va a resultar muy difícil que el alumno salga adelante.</p>
+        <br>
+        <p>El papel más importante que tienen que cumplir los padres de los niños
+            disléxicos quizás sea el de apoyo emocional y social. También es importante
+            comunicarle que se le seguirá queriendo, aunque no pueda ir especialmente bien en
+            el colegio. Aunque es muy difícil, hay que evitar que la ansiedad de los padres
+            aumente los problemas del niño, incrementando su angustia y preocupación y
+            generando dificultades emocionales secundarias.</p>
+        <br>
+        <p>Los padres (y todos los que se relacionan con él o ella) deben dejar muy claro
+            al niño que puede tener éxito, ya que si el niño “sabe” que no lo puede tener
+            (porque así se lo hacen sentir las personas importantes de su entorno) tendrá miedo
+            a intentarlo; y como en la profecía que se auto-cumple, hace por fracasar, sin
+            apenas darse cuenta. Eso complica la tarea del especialista.</p>
+        <br>
+        <p>Es importante desarrollar la autoestima a todos los niveles. Puede hacerse
+            dispensando al niño consideración positiva incondicional, en especial cuando se
+            siente decaído o fracasado. Es fundamental evaluarlo con su propio nivel, esfuerzo y
+            rendimiento. La dificultad radica en no pasar a la sobreprotección, al “todo vale”.
+            Pero la guía es tener clara la escala de valores en la que se desenvuelve el niño, la
+            situación de partida, el esfuerzo realizado.</p>
+        <br>
+        <p>Otro aspecto a tener en cuenta son las dificultades prácticas asociadas con
+            la dislexia: confusiones con las horas del día, equivocaciones respecto del lugar
+            donde se colocan las cosas, tendencia al desorden, facilidad para distraerse, torpeza
+            en ocasiones, dificultad en el cumplimiento de las instrucciones, etc. Todas ellas son
+            conductas de por sí enervantes, que pueden acabar con los nervios más templados;
+            sólo la información precisa de lo que significa e implica la dislexia los podrá ayudar a
+            sobrellevar su vida diaria. Cuando comprendan qué puede y qué no puede hacer el
+            niño en cada momento podrán relajarse y atenderlo en lo que más le convenga. Con
+            un disléxico hay que aprender a convivir.</p>
+        <br>
+        <ul>
+            <li> Ayudarle a organizar el orden del trabajo a desarrollar (empezar con las
+                asignaturas de dificultad superior, después las más sencillas para él y,
+                finalmente, las más mecánicas). Tener siempre en cuenta:
+                <ul>
+                    <li>
+                        Lo que es para el día siguiente y lo que no.
+                    </li>
+                    Las tareas que en los siguientes días no se podrán hacer por
+                    causa de las extraescolares.
+                    <li>
+                        Los exámenes programados (nunca se pueden dejar para el
+                        último día, porque carecen de memoria a corto plazo).
+                    </li>
+                    <li>
+                        Las tareas de larga duración (lectura de libros, trabajos, etc.).
+                    </li>
+                </ul>
+            </li>
+            <li>Dejarle solo en lo que pueda hacer de manera autónoma, pero estar a su
+                lado en lo que no. Tiene que saber que estamos ahí, por si nos necesita.
+            </li>
+            <li>
+                Cuando el niño está agotado, y es evidente que no rinde, tomarle el lápiz y
+                continuar escribiendo (él nos dicta y nosotros transcribimos). A esto se lo
+                conoce como “hacer de secretario”.
+            </li>
+            <li>
+                Cuando ni así dé resultado, poner una nota al profesor que, previamente
+                informado del problema, tendrá que aceptar nuestra decisión de terminar con
+                los deberes del día.
+                No dudar de poner notas justificativas cuando alguna tarea es inasequible o
+                absurda para él (lecturas largas con poco tiempo, copiar lecciones, copiar
+                reiterativamente palabras erróneas, etc.) o, si sabemos que le acarrearía
+                consecuencias indeseadas, hacérsela directamente nosotros.
+            </li>
+            <li>
+                Leer con él los libros y otras tareas que le han dado, o leérselos
+                directamente. Es importante que aprenda y se entere de lo que lee, por lo
+                tanto habrá que explicarle el significado de las palabras nuevas y lo que está
+                pasando en el texto.
+            </li>
+            <li>
+                Si resulta demasiado complicado utilizar diccionarios y agendas
+                convencionales o si se pierde demasiado tiempo, habrá que explorar y
+                enseñarle a utilizar instrumentos electrónicos como las agendas electrónicas
+                y los correctores de ortografía o los diccionarios y los calendarios
+                informáticos.
+            </li>
+            <li>
+                La actitud de los padres debe basarse en el sentido común. Si el niño hace
+                una pregunta sobre ortografía o gramática cuando está escribiendo, se le da
+                la respuesta y que siga con su tarea. Esto también es válido para las
+                matemáticas; los niños disléxicos a menudo tienen problemas con la memoria
+                repetitiva. Mejor darle la respuesta si se ve que el niño sabe hacer el cálculo.
+            </li>
+        </ul>
+        <h3 class="subtitulo">Su papel en la escuela</h3>
+        <br>
+        <p>En este aspecto, habría muchas cosas a decir, pero quizá lo más importante
+            sería:</p>
+        <ul>
+            <li>
+                Prepare una lista de las recomendaciones del psicopedagogo
+                y entregue
+                una copia a cada profesor de su hijo; de esta manera tendrá la certeza de que
+                todos están debidamente informados.
+            </li>
+            <li>
+                Mantenga un contacto regular con los profesores. Intente lograr que
+                familiaricen a los compañeros de su hijo con lo que significa ser disléxico y
+                que les expliquen cómo pueden ayudar. Hágalo Vd. mismo con los amigos del
+                niño y con sus familias.
+            </li>
+            <li>
+                Utilice códigos de color para marcar todos los libros y bolsas, así su hijo los
+                reconocerá inmediatamente.
+            </li>
+            <li>
+                Procure que todo su material escolar esté siempre preparado y ordenado en
+                el mismo lugar.
+            </li>
+            <li>
+                Enséñele a su hijo a preparar y vaciar su cartera y a organizar su estuche
+                de lápices. No dé por sentado que adquirirá estas aptitudes por sí mismo.
+            </li>
+            <li>
+                Mantenga un registro del tiempo que dedica su hijo a hacer sus deberes y
+                comparta esta información con el profesor ya que posiblemente no esté al
+                corriente del tiempo que necesita.
+            </li>
+        </ul>
+        <br>
+        <h3 class="subtitulo">Su necesidad de un entorno seguro y estable</h3>
+        <br>
+        <p>Los niños con dificultades de aprendizaje prosperan mejor en entornos estables y seguros, donde la estructura y la rutina son fundamentales. Adaptarse a cambios, como trasladarse a otro lugar, puede ser especialmente desafiante para ellos, ya que dependen de la familiaridad y la repetición para sentirse seguros, incluso en detalles aparentemente insignificantes como conocer el camino a la escuela.</p>
+        <br>
+        <p>Una vez que estos niños aprenden algo en un entorno estable, lo retienen de manera duradera. Sin embargo, es esencial que quienes los rodean, incluidos educadores y cuidadores, reconozcan y acepten sus dificultades específicas de aprendizaje, y estén dispuestos a comunicarse de manera que les ayude a comprender y superar los desafíos educativos.</p>
+        <br>
+        <p>Es crucial no asumir que estos niños no pueden cumplir con las expectativas escolares, sino más bien reconocer que el sistema educativo debe adaptarse para satisfacer sus necesidades individuales y proporcionarles una enseñanza que sea accesible y efectiva para ellos.</p>
+        <br>
+        <h3 class="subtitulo">Aceptar la realidad</h3>
+        <br>
+        <p>Cuando se comunica a los padres que su hijo tiene un trastorno del aprendizaje, suelen experimentar una mezcla de emociones negativas como negación, rencor, temor, entre otras. Es crucial que acepten la situación para poder elaborar estrategias positivas que ayuden al niño a desarrollar sus capacidades plenamente.</p>
+        <p>Es fundamental que los padres reconozcan y acepten la dislexia de su hijo, ya que esto facilita la elección de asignaturas adecuadas y permite solicitar adaptaciones curriculares necesarias. Esto incluye evaluar si la escuela actual está proporcionando el apoyo adecuado o si sería mejor considerar un cambio.</p>
+        <p>Es recomendable diagnosticar las dificultades de aprendizaje lo antes posible para acceder a la ayuda adecuada y aumentar las posibilidades de superar las dificultades. Los padres deben confiar en su intuición y buscar evaluaciones profesionales si sospechan problemas educativos.</p>
+        <p>Crear un ambiente seguro y positivo en casa es crucial, especialmente porque la escuela puede resultar desalentadora para el niño. Fomentar cualquier talento especial del niño y elogiar sus logros son estrategias importantes para construir su autoestima.
+        </p>
+        <p>Es importante también involucrar al niño en las conversaciones sobre sus dificultades de aprendizaje de manera positiva y constructiva, y buscar apoyo en grupos de padres y organizaciones que puedan ofrecer información útil y apoyo emocional.
+        </p>
+        <p>Los padres de niños disléxicos, harían bien en averiguar qué grupos de
+            apoyo y otras organizaciones relevantes se encuentran en su zona. A veces es un
+            alivio ver que su familia no es la única que sobrelleva la vida con un hijo con
+            dificultades de aprendizaje; no sólo les ofrecerán su apoyo, sino que también podrán
+            obtener informaciones muy útiles. Lo mejor es buscar en Internet la asociación más
+            cercana a nuestro lugar de residencia.</p>
+    </div>
+</section>
+
+<section class="container ref">
+        <h1>Referencias</h1>
+        <ul class="list">
+            <li>
+                GUÍA GENERAL SOBRE DISLEXIA 2.010 ASOCIACIÓN ANDALUZA DE DISLEXIA (ASANDIS). (n.d.). https://www.disfam.org/wp-content/uploads/2017/03/guia-general-sobre-dislexia-andalucia.pdf         
+            </li>
+        </ul>
     </section>
 
-    <?php include("template/footer.php")?>
+<?php include("template/footer.php") ?>
