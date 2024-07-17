eb8005abfa2d1708b48f5fe7202f42ba71ac2289
eglezg <ernestoglezg9182@hotmail.com>
2024-07-17 12:45:36 -0600

    Diseño reporte

--- reporte.php
+++ reporte.php
@@ -16,10 +16,10 @@ if ($conn->connect_error) {
 // Obtén el id del usuario desde la URL o la sesión
 $idUsuario = isset($_GET['id']) ? $_GET['id'] : (isset($_SESSION["idUsuario"]) ? $_SESSION["idUsuario"] : null);
 
-if ($idUsuario === null) {
+/*if ($idUsuario === null) {
     header("Location: prueba-registro.php");
     exit();
-}
+}*/
 
 // Obtén la información del usuario
 $query = "SELECT * FROM usuario WHERE idUsuario = ?";
@@ -39,43 +39,48 @@ $result = mysqli_stmt_get_result($stmt);
 $user_report_info = mysqli_fetch_assoc($result);
 mysqli_stmt_close($stmt);
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reporte</title>
-</head>
+include("template/header.php") ?>
 
 <body>
-    <div class="content">
-            <h2>INFORMACIÓN</h2>
-        <table>
-            <tr>
-                <th>Nombre</th>
-                <th>Email</th>
-                <th>Fecha</th>
-                <th>Memorama</th>
-                <th>Patrones</th>
-                <th>Palabra-Imagen</th>
-                <th>Completa Palabra</th>
-                <th>Tiempo</th>
-            </tr>
-            <tr>
-                <td><?php echo isset($user_info['nombre']) ? $user_info['nombre'] : ""; ?></td>
-                <td><?php echo isset($user_info['correo']) ? $user_info['correo'] : ""; ?></td>
-                <td><?php echo isset($user_report_info['fecha']) ? $user_report_info['fecha'] : ""; ?></td>
-                <td><?php echo isset($user_report_info['prueba1']) ? $user_report_info['prueba1'] : ""; ?></td>
-                <td><?php echo isset($user_report_info['prueba2']) ? $user_report_info['prueba2'] : ""; ?></td>
-                <td><?php echo isset($user_report_info['prueba3']) ? $user_report_info['prueba3'] : ""; ?></td>
-                <td><?php echo isset($user_report_info['prueba4']) ? $user_report_info['prueba4'] : ""; ?></td>
-                <td><?php echo isset($user_report_info['tiempo']) ? $user_report_info['tiempo'] : ""; ?></td>
-            </tr>
-        </table>
+    <div class="contenedor-rep">
+        <div class="contenido-rep">
+            <div class="encabezado-rep">
+                <img src="images/Logo.png" class="encabezado-img img-fluid">
+                <h2>Dislexia Kids Pre-Diagnóstico de Dislexia en niños
+                    de 1ro y 2do grado de primaria.</h2>
+            </div>
+            <hr>
+            <h1>Reporte</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <h3>Resultados</h3>
+            <div class="cont-tabla">
+                <div class="table-responsive">
+                    <table class="tabla-result">
+                        <tr>
+                            <th>Memorama</th>
+                            <th>Patrones</th>
+                            <th>Palabra-Imagen</th>
+                            <th>Completa Palabra</th>
+                            <th>Tiempo</th>
+                            <th>Resultado Final</th>
+                        </tr>
+                        <tr>
+                            <td><?php echo isset($user_report_info['prueba1']) ? $user_report_info['prueba1'] : ""; ?></td>
+                            <td><?php echo isset($user_report_info['prueba2']) ? $user_report_info['prueba2'] : ""; ?></td>
+                            <td><?php echo isset($user_report_info['prueba3']) ? $user_report_info['prueba3'] : ""; ?></td>
+                            <td><?php echo isset($user_report_info['prueba4']) ? $user_report_info['prueba4'] : ""; ?></td>
+                            <td><?php echo isset($user_report_info['tiempo']) ? $user_report_info['tiempo'] : ""; ?></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <h3>Pre-Diagnóstico</h3>
+            <p class="pre-diag">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </div>
 
-</body>
-
-</html>
+    <?php include("template/footer.php") ?>
